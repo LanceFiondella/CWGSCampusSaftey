a574adaf84d6cad976b699cbd8c646648a40f1bc
Christian Ellis <christian.ellis96@gmail.com>
2017-07-19 21:53:10 -0400

    Working Open Native Apps solution, still have to expand to whole app

--- src/providers/native-device-features.ts
+++ src/providers/native-device-features.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import {DomSanitizer} from '@angular/platform-browser';
+
 
 @Injectable()
 export class NativeDeviceFeatures {
-  constructor(){
 
+  constructor(private platform: Platform, private sanitizer:DomSanitizer){
+    
   }
 
   onOpenDialer(phoneNumber: string){
     return "tel:" + phoneNumber;
   }
 
+  onOpenMaps(location: string){
+    let temp = "";
+
+    if(this.platform.is("ios")){
+      temp = "maps:?q=" + location;
+      return this.sanitizer.bypassSecurityTrustUrl(temp);
+    } 
+    else{
+      temp = "geo:?q=" + location;
+      return this.sanitizer.bypassSecurityTrustUrl(temp);
+    }
+  }
 }
