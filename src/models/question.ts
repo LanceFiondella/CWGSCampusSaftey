457f6dab00310012e8f724a76b7d24ac56a7feaa
Christian Ellis <christian.ellis96@gmail.com>
2017-11-12 17:11:27 -0500

    Updated Q&A tab

--- src/models/question.ts
+++ src/models/question.ts
@@ -1,11 +1,8 @@
 export class Question {
-
-  id: string;
   question: string;
   answer: string;
 
   constructor(q: Question) {
-      this.id = q.id;
       this.question = q.question;
       this.answer = q.answer;
   }
