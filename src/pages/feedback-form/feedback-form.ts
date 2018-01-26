6f6bf1febf766aefab0ebcadd330d8f924ae3b5e
Christian Ellis <christian.ellis96@gmail.com>
2018-01-25 20:28:44 -0500

    Start of Front-End Feedback Form

--- src/pages/feedback-form/feedback-form.ts
+++ src/pages/feedback-form/feedback-form.ts
@@ -1,12 +1,36 @@
+//Library Imports
 import { Component } from '@angular/core';
-import { HTTP } from '@ionic-native/http';
-
+//For HTTP
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/of';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'page-feedback-form',
-  templateUrl: 'feedback-form.html'
+  templateUrl: 'feedback-form.html',
+
 })
 
 export class FeedbackFormPage {
-Htt
+  disabled: boolean;
+  userInput: String;
+  postObservable: Observable<any>;
+
+  constructor(private http: HttpClient) { }
+
+  sendData(){
+    this.disabled = true;
+    console.log(this.userInput);
+    let body = {
+        message: this.userInput
+    };
+    this.postObservable = this.http.post('http://localhost:5400/api/feedbackform', body)
+    .catch((err) => {
+      console.log(err);
+      return Observable.of(err);
+    })
+    this.postObservable.subscribe();
+  }
 }
