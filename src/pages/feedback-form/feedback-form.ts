1e0ac6f00fb5223e61d84b35f48a2c46753a0a6d
Christian Ellis <christian.ellis96@gmail.com>
2018-01-28 14:06:20 -0500

    Added a find platform method, and changed post body to match model in backend

--- src/pages/feedback-form/feedback-form.ts
+++ src/pages/feedback-form/feedback-form.ts
@@ -1,5 +1,7 @@
 //Library Imports
 import { Component } from '@angular/core';
+import { NativeDeviceFeatures } from '../../providers/native-device-features';
+
 //For HTTP
 import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
@@ -7,30 +9,31 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/of';
 import { async } from '@angular/core/testing';
 
+
 @Component({
   selector: 'page-feedback-form',
   templateUrl: 'feedback-form.html',
-
 })
-
 export class FeedbackFormPage {
   disabled: boolean;
   userInput: String;
-  postObservable: Observable<any>;
+  postRequestObservable: Observable<any>;
+  userPlatform: String;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private nativeDeviceFeatures:NativeDeviceFeatures) { }
 
   sendData(){
     this.disabled = true;
     console.log(this.userInput);
     let body = {
-        message: this.userInput
+        message: this.userInput,
+        sentFrom: "Sent from " + this.nativeDeviceFeatures.getPlatform() + " Ionic App"
     };
-    this.postObservable = this.http.post('http://localhost:5400/api/feedbackform', body)
+    this.postRequestObservable = this.http.post('http://localhost:5400/api/feedbackform', body)
     .catch((err) => {
       console.log(err);
       return Observable.of(err);
     })
-    this.postObservable.subscribe();
+    this.postRequestObservable.subscribe();
   }
 }
--- src/providers/native-device-features.ts
+++ src/providers/native-device-features.ts
@@ -1,29 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import {DomSanitizer} from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
 
 
 @Injectable()
 export class NativeDeviceFeatures {
+  userPlatform: String;
 
-  constructor(private platform: Platform, private sanitizer:DomSanitizer){
+  constructor(private platform: Platform, private sanitizer:DomSanitizer) { }
 
-  }
-
+  //* Open the native dialer
   onOpenDialer(phoneNumber: string) {
     return "tel:" + phoneNumber;
   }
 
+  // *Open the native maps app
   onOpenMaps(location: string, locationName?:string) {
     let temp = "";
     let ifLocationName = "";
+    this.userPlatform = this.getPlatform();
+
    //if on ios
-    if(this.platform.is("ios")) {
-      //?ll=location
+    if(this.userPlatform == "ios") {
       temp = "maps:?q=" + location;
       return this.sanitizer.bypassSecurityTrustUrl(temp);
     }
-    //else must be android
+    //else must be android or web(dev only)
     else {
       //if a location name is provided, add it to the google maps intent URI
       if(locationName){
@@ -31,6 +33,22 @@ export class NativeDeviceFeatures {
       }
       temp = "geo:?q=" + location + ifLocationName;
       return this.sanitizer.bypassSecurityTrustUrl(temp);
+    }
+  }
+
+  // *Find what platform the user is on
+  getPlatform(){
+    if(this.platform.is('mobileweb')){
+      return this.userPlatform = 'mobileweb';
+    }
+    else if(this.platform.is('ios')) {
+      return this.userPlatform = 'ios';
+    }
+    else if(this.platform.is('android')) {
+      return this.userPlatform = 'android';
+    }
+    else {
+      return this.userPlatform = 'unknown';
     }
   }
 }
